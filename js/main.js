8cd93c3d278c5a97019fd398963b2871bc8e98df
Juankrc <jcrodriguez488@gmail.com>
2024-10-02 16:37:49 -0500

    V5

--- js/main.js
+++ js/main.js
@@ -34,24 +34,26 @@ const swiper = new Swiper('.slider-wrapper', {
 
   src="https://cdn.jsdelivr.net/npm/emailjs-com@2.6.4/dist/email.min.js"
 
-  // Para enviar email con EmailJS 
-  (function() {
-      emailjs.init("_stV40RaoZI2LMlYP"); //Es el ID de usuario que te proporciona EmailJS al registrarte.
-  })();
+// Inicializar EmailJS con el ID de usuario
+(function() {
+  emailjs.init("_stV40RaoZI2LMlYP"); // Asegúrate de que sea el ID correcto
+})();
 
-  // funcion de enviar los datos del formulario
+// Función para enviar los datos del formulario
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe de manera tradicional
 
-  document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita el comportamiento predeterminado del formulario
+  // Mostrar mensaje de depuración
+  console.log('Intentando enviar el formulario...');
 
-    // Enviar los datos del formulario usando EmailJS
-    emailjs.sendForm('service_09bvgxa', 'template_fg56m5e', this)
-        .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-            document.getElementById("respuesta").innerText = "Mensaje enviado correctamente.";
-        }, function(error) {
-            console.log('FAILED...', error);
-            document.getElementById("respuesta").innerText = "Error al enviar el mensaje.";
-        });
+  // Enviar los datos del formulario usando EmailJS
+  emailjs.sendForm('service_09bvgxa', 'template_fg56m5e', this)
+      .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          document.getElementById("respuesta").innerText = "Mensaje enviado correctamente.";
+      }, function(error) {
+          console.log('FAILED...', error);
+          document.getElementById("respuesta").innerText = "Error al enviar el mensaje.";
+      });
 });
 
